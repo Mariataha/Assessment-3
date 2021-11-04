0e747876d905fe9898d878d3ba32eb77822e800b
Mariataha <mariat2010@gmail.com>
2021-11-04 14:05:09 -0500

    Added Neighborhood html

--- neighborhood.js
+++ neighborhood.js
@@ -1,6 +1,8 @@
 /*Extra Credit: The page should feature a short description of your neighborhood, links to local restaurants and parks, and anything else you’d like to include.*/
 
-class Restaurant {
-    
+let restaurantArray = ["Chipotle", "Thai Village", "El Pastor"]
 
-}
+
+
+//The page should feature a short description of your neighborhood, links to local restaurants and parks, and anything else you’d like to include.
+
