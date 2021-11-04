72a9d386316c5cbfa147a77691db0667d8d0fbe7
Mariataha <mariat2010@gmail.com>
2021-11-04 02:27:56 -0500

    First commit

---
+++ neighborhood.js
@@ -0,0 +1,6 @@
+/*Extra Credit: The page should feature a short description of your neighborhood, links to local restaurants and parks, and anything else you’d like to include.*/
+
+class Restaurant {
+    
+
+}
---
+++ profile.js
@@ -0,0 +1,20 @@
+//In profile.js, create event listeners that check for a click on each of these buttons.
+//If a user clicks on “My Favorite Color”, it should alert the user of your favorite color, and so on.
+
+
+const buttons = document.querySelectorAll(".favorites");
+buttons. 
+
+
+const Maria = {
+    "color": "Blue",
+    "place": "Tokyo",
+    "ritual": "Doing my skincare every night.",
+}
+
+//I don't know how to do the rest of this
+
+function eventListener(event){
+
+
+}
